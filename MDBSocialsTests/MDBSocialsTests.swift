d908e001a1cc82c8551d8c11cfda715b4e540ebe
Kayli Jiang <kaylijiang@gmail.com>
2018-09-27 21:27:40 -0700

    Initial Commit

---
+++ MDBSocialsTests/MDBSocialsTests.swift
@@ -0,0 +1,36 @@
+//
+//  MDBSocialsTests.swift
+//  MDBSocialsTests
+//
+//  Created by Kayli  Jiang on 9/27/18.
+//  Copyright © 2018 Kayli  Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import MDBSocials
+
+class MDBSocialsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
